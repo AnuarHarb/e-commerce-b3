9cc0a03012918b2a70b75f82ac1b183a623b1752
Anuar Harb <anuarharb@gmail.com>
2025-06-04 20:53:01 -0500

    estados en react

---
+++ app/app.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Card } from "./components/card";
+import styled from 'styled-components'
+
+const AppContainer = styled.section`
+  .card-container {
+    display: flex;
+    gap: 10px;
+  }
+`
+
+const App = () => {
+  return (
+    <AppContainer>
+      <h1>My React app</h1>
+      <section className="card-container">
+        <Card 
+          titulo="Nike Air Force 1 '07" 
+          descripcion="Cómodas, duraderas y atemporales: son las número 1 por una razón. El diseño clásico de los 80 se combina con piel lisa y detalles llamativos para conseguir un estilo perfecto tanto en la cancha como fuera de ella."
+          precio="119000"
+        />
+        <Card 
+          titulo="Air Max 95 OG"
+          descripcion="Grandes. Llamativas. Reactivas. Celebra el 30 aniversario de un modelo icónico con la edición Big Bubble de estas Air Max del 95. Estas zapatillas se mantienen fieles a las originales y añaden altura en la parte superior con unas capas de piel sintética y malla transpirable en forma de onda que van cambiando de negro a blanco. Los toques de color bright mandarin dan el extra perfecto en las presillas de los cordones, los logotipos y las unidades Air. Eleva tu estilo con más Air todavía y disfruta del futuro con tu experiencia Air Max." 
+          precio="189000"
+        />
+        <Card 
+          titulo="Nike Pegasus 41"
+          descripcion="La amortiguación reactiva de las Pegasus ofrece una pisada enérgica para el running diario sobre asfalto. Gracias a las unidades Air Zoom dobles y la mediasuela de espuma ReactX, disfrutarás de un retorno de energía con mayor ligereza. La malla Engineered Mesh mejorada de la parte superior reduce el peso y aumenta la transpirabilidad."
+          precio="149000"
+        />
+      </section>
+    </AppContainer>
+  );
+};
+
+export default App;
---
+++ app/components/card.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import styled from 'styled-components'
+
+const StyledCard = styled.article`
+  border: 1px solid black;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+
+  .titulo {
+    font-size: 25px;
+  }
+
+  img {
+    width: 100%;
+  }
+
+  .card-descipcion-container {
+    padding: 10px;
+  }
+
+  .activo {
+    background-color: gray;
+  }
+
+  .card-checkout-buttons {
+    display: flex;
+    justify-content: space-between;
+
+    input {
+      width: 50px;
+    }
+  }
+
+  .card-descripcion-truncade {
+    height: 50px;
+    overflow: hidden;
+  }
+`;
+
+
+export const Card = ({titulo, descripcion, precio}) => {
+  const [cuenta, setCuenta] = useState(0);
+  const [enCarrito, setEnCarrito] = useState(false);
+  const [mostrarDescripcion, setmostrarDescripcion] = useState(false)
+
+  const actualizarCuenta = (tipo) => {
+    if(cuenta > 0 && tipo === "resta") {
+      setCuenta(cuenta - 1)
+    }
+
+    if(tipo === 'suma') {
+      setCuenta(cuenta + 1)
+    }
+  }
+
+  return (
+    <StyledCard>
+      <img src="/public/zapatos.webp"></img>
+      <div className={`card-descipcion-container ${enCarrito ? 'activo' : ''}`}>
+        <h2 className="titulo">{titulo} - {precio}</h2>
+
+        
+        {mostrarDescripcion ? (
+          <p className="card-descripcion-full">{descripcion}</p>
+        ) : (
+          <div>
+            <p className="card-descripcion-truncade">{descripcion}</p>
+            <button onClick={() => setmostrarDescripcion(true)}>Ver descripción completa</button>
+          </div>
+        )}
+
+
+
+        <div className="card-checkout-buttons">
+          <div>
+            <button onClick={() => actualizarCuenta('resta')}>-</button>
+            <input type="number" value={cuenta}/>
+            <button onClick={() => actualizarCuenta('suma')}>+</button>
+          </div>
+          <button onClick={() => setEnCarrito(true)}>Añadir al carrito</button>
+        </div>
+      </div>
+
+
+    </StyledCard>
+  );
+};
